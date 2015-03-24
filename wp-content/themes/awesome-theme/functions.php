e4e74c4e1be30e6b4ff59c73568383997fbaa9b7
jfrisch01 <jfrisch01@gmail.com>
2015-03-23 21:18:48 -0700

    day 3 awesome theme

---
+++ wp-content/themes/awesome-theme/functions.php
@@ -0,0 +1,25 @@
+<?php
+//use this file for custom functions and activating 'sleeping' wordpress features
+//allow you to attach a "featured image" to each post or page
+add_theme_support('post-thumbnails');
+
+add_image_size('post-thumbnails');
+
+add_image_size( 'big-banner', 1300, 300, true );
+
+
+
+
+//no close PHP!
+
+
+
+
+
+
+
+
+
+
+
+
